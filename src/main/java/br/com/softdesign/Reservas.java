e88cc95b1e7244d75718abddadfd8e961ae48236
luis.pires <luispiresqa@gmail.com>
2020-09-02 11:20:28 -0300

    Adicionando a pasta src

---
+++ src/main/java/br/com/softdesign/Reservas.java
@@ -0,0 +1,13 @@
+package br.com.softdesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class Reservas
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
